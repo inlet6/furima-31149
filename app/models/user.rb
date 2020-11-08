d25f0c230c9489d71181ffce86a85c21298cd41f
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-08 18:39:31 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nick_name, presence: true
-         validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-
-         validates :encrypted_password, confirmation: true, length: { minimum: 6 }
-         validates :first_name, presence: true
-         validates :family_name, presence: true
-         validates :first_name_kana, presence: true, format: { with: [ァ-ン] }
-         validates :family_name_kana, presence: true, format: { with: [ァ-ン] }
-         validates :birthday , presence: true
-
+  with_options presence: true
+    validates :nick_name
+    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :encrypted_password, confirmation: true, length: { minimum: 6 }
+    validates :first_name
+    validates :family_name
+    with_options format: { with: [ァ-ン] }
+      validates :first_name_kana
+      validates :family_name_kana
+    end
+    validates :birthday
+  end
 end
