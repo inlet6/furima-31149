8c5816c1bca0d1561d957c01dc7ce072d1084bfd
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-09 18:32:51 +0900

    ユーザー管理機能実装　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nick_name
     validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :encrypted_password, confirmation: true, length: { minimum: 6 }
+    validates :password, confirmation: true, length: { minimum: 6 }
 
     with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
       validates :first_name
