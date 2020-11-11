140874af4734b53ea1d1d586dcfa04f778e445ae
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-11 18:40:32 +0900

    link、ルーティングの指定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,4 +14,5 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:image, :name, :details, :category_id, :item_states_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price, :user)
+  end
 end
