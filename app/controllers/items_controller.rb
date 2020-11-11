6c2024f37b46a85dd7ac5aceea4e9721e81a48a8
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-11 18:27:13 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,10 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :details, :category_id, :item_states_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price, :user)
 end
