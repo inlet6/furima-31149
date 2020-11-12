301d5b652ff469b49167fa52a03fc6d9ea239b0c
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-12 20:25:54 +0900

    くリエイトアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    if current_user.created_at
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
       redirect_to root_path
     else
       render :new
@@ -19,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :details, :category_id, :item_status_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price, :user)
+    params.require(:item).permit(:image, :name, :details, :category_id, :item_status_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 end
