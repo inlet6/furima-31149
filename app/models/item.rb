a3b5bc79e63f1efc28dbd921cf236cf6e89ec69e
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-13 20:04:49 +0900

    javascriptの記述。税、利益の算出

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,17 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :days_to_ship, :item_status, :prefecture, :shipping_burden
+
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :details, length: { maximum: 1000 }
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_burden_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+    validates :price, numericality: { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 }
+    validates :user
+  end
 end
