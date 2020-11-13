025123344687b8b5b02786cb14be0e11a4e6ef6d
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-13 20:53:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,11 +8,13 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :details, length: { maximum: 1000 }
-    validates :category_id
-    validates :item_status_id
-    validates :shipping_burden_id
-    validates :prefecture_id
-    validates :days_to_ship_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_burden_id
+      validates :prefecture_id
+      validates :days_to_ship_id
+    end
     validates :price, numericality: { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 }
     validates :user
   end
