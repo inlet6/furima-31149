f2e40a58f9fd1987420eff554588a18c856b9310
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-13 20:47:32 +0900

    モデルコード　ファクトリーの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :item do
+    name               {apex}
+    image              {image_tag '/images/flag.png'}
+    details            {cool}
+    category_id        {2}
+    item_status_id     {2}
+    shipping_burden_id {2}
+    prefecture_id      {2}
+    days_to_ship_id    {2}
+    price              {1000}
   end
 end
