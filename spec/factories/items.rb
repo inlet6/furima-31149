5dd53b36bf4f7997600ec6ffdd182a2e20cde44d
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-14 17:54:48 +0900

    テストコードの記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,18 @@
 FactoryBot.define do
   factory :item do
-    name               {apex}
-    image              {image_tag '/images/flag.png'}
-    details            {cool}
+    association :user
+
+    name               {Faker::Lorem.characters(41)}
+    details            {Faker::Base.regexify("[aあ]{1001}")}
     category_id        {2}
     item_status_id     {2}
     shipping_burden_id {2}
     prefecture_id      {2}
     days_to_ship_id    {2}
     price              {1000}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/flag.png'), filename: 'flag.png')
+    end
   end
 end
