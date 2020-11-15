de36498214135388b022c72c2a840f2de87970d6
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-15 19:11:56 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,22 +62,22 @@ describe Item do
         expect(@item.errors.full_messages).to include('Details is too long (maximum is 1000 characters)')
       end
       it 'category_idが1だと登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it 'shipping_burden_idが1だと登録できない' do
-        @item.shipping_burden_id = '1'
+        @item.shipping_burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping burden must be other than 1')
       end
       it 'prefecture_idが1だと登録できない' do
-        @item.prefecture_id = '1'
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it 'days_to_ship_idが1だと登録できない' do
-        @item.days_to_ship_id = '1'
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Days to ship must be other than 1')
       end
@@ -87,12 +87,12 @@ describe Item do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'priceが300以下だと登録できない' do
-        @item.price = '1'
+        @item.price = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
       it 'priceが9999999以上だと登録できない' do
-        @item.price = '9999999999'
+        @item.price = 9999999999
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
