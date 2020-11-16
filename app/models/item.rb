639d851896a55b2051383a6960162f26c3a51a53
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-16 12:58:46 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :days_to_ship, :item_status, :prefecture, :shipping_burden
+  belongs_to :category
+  belongs_to :days_to_ship
+  belongs_to :item_status
+  belongs_to :prefecture
+  belongs_to :shipping_burden
 
   with_options presence: true do
     validates :image
