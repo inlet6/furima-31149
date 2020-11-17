e07c167890e85f37134c22acba5efcdf28acb243
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-17 20:11:00 +0900

    buy_infosコントローラー記述

---
+++ app/controllers/buy_infos_controller.rb
@@ -0,0 +1,34 @@
+class BuyInfosController < ApplicationController
+  before_action :set_item, only: [ :index ]
+
+  def index
+    @buy_infos = BuyInfo.new
+
+  end
+
+  def create
+    @buy_info = BuyInfo.new(buy_info_params)
+    if @buy_info.valid?
+      @buy_info.save
+      return redirect_to root_path
+    else
+      render 'index'
+      
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :details, :category_id, :item_status_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def buy_info_params
+    params.require(:buy_info).permit(:user, :item)
+  end
+
+end
