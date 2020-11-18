91beebc236997f82df81eefca0c22214f2981fef
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-18 20:26:54 +0900

    コントローラーの記述

--- app/controllers/buy_infos_controller.rb
+++ app/controllers/buy_infos_controller.rb
@@ -1,14 +1,14 @@
 class BuyInfosController < ApplicationController
-  before_action :set_item, only: [:index]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @buy_info = BuyInfo.new
+    @pay_form = PayForm.new
   end
 
   def create
-    @buy_info = BuyInfo.new(buy_info_params)
-    if @buy_info.valid?
-      @buy_info.save
+    @pay_form = PayForm.new(pay_form_params)
+    if @pay_form.valid?
+      @pay_form.save
       redirect_to root_path
     else
       render 'index'
@@ -18,15 +18,11 @@ class BuyInfosController < ApplicationController
 
   private
 
-  def item_params
-    params.require(:item).permit(:image, :name, :details, :category_id, :item_status_id, :shipping_burden_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
-  end
-
   def set_item
     @item = Item.find(params[:item_id])
   end
 
-  def buy_info_params
-    params.require(:buy_info).permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :buy_info_id)
+  def pay_form_params
+    params.require(:pay_form).permit(:user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :buy_info)
   end
 end
