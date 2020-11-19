2dc96251da13d1501f39e448222e914d6f1293cf
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-19 16:19:19 +0900

    card.js記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey(pk_test_cb734ef92ca66668f928e863);
+  Payjp.setPublicKey("pk_test_cb734ef92ca66668f928e863");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -7,11 +7,18 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get('number'),
-      cvc: formData.get('cvc'),
-      exp_month: formData.get('exp_month'),
-      exp_year: `20${formData.get('exp_year')}`,
+      number: formData.get('pay_form[number]'),
+      cvc: formData.get('pay_form[cvc]'),
+      exp_month: formData.get('pay_form[exp_month]'),
+      exp_year: `20${formData.get('pay_form[exp_year]')}`,
     };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
