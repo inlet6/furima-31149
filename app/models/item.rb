c124ae7075478cd687e08c83352e1573d556ff9f
inlet6 <72550990+inlet6@users.noreply.github.com>
2020-11-20 10:53:23 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_one :buy_info
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :days_to_ship
